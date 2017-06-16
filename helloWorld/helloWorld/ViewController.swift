3224fd1d72dfa2a4ee62c1d2379f558046c2da9e
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-06-16 21:42:32 +1000

    added a label, a button and a text field, all with user functionality.

--- helloWorld/helloWorld/ViewController.swift
+++ helloWorld/helloWorld/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        label.text = textField.text
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
